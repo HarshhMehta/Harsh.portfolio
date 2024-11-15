4ea75e0571cbef46f41cec9d9a5f128bead03f11
Harsh Mehta <92149953+HarshhMehta@users.noreply.github.com>
2024-11-15 19:17:36 +0530

    Update Home.jsx

--- src/Components/Content/Home.jsx
+++ src/Components/Content/Home.jsx
@@ -34,7 +34,7 @@ function Home() {
     <div id='Home' className=' bg-white text-black dark:bg-slate-900 dark:text-white lg:px-56 lg:py-0 px-10 py-20 text-center gap-5 lg:text-start flex lg:flex-row flex-col-reverse justify-between lg:gap-28 items-center'>
       <div className='h-full lg:py-40 flex flex-col justify-center lg:items-start items-center '>
         <h1 data-aos='fade-right' className='text-[52px] font-semibold mb-8 leading-normal uppercase'>
-          Hola! <span className='text-fuchsia-500'>Harsh Here..</span>
+          Hello! <span className='text-fuchsia-500'>Harsh Here..</span>
         </h1>
         <p data-aos='fade-left'>Hey folks! I'm a dedicated Front-end developer Based in Indore, currently in Vadodara, India. With a passion for crafting stunning and interactive web experiences, I am constantly expanding my expertise in web development. With a diverse skill set, I aim to contribute innovatively to every project.</p>
         <div data-aos='fade-up' className='flex mt-8 gap-2'>

6ada85a40c575185aee6d9c865516657e5dec40c
Harsh Mehta <92149953+HarshhMehta@users.noreply.github.com>
2025-02-11 17:45:27 +0530

    Add Projects

--- src/All_Lists/ProjectsList.js
+++ src/All_Lists/ProjectsList.js
@@ -1,8 +1,12 @@
-import img1 from '../assets/To-do.png'
-import img2 from '../assets/weather.png'
-import img3 from '../assets/Intract.png'
-import img4 from '../assets/Home-care.png'
-import img5 from '../assets/Notion.png'
+import img1 from '../assets/To-do.png';
+import img2 from '../assets/weather.png';
+import img3 from '../assets/Intract.png';
+import img4 from '../assets/Home-care.png';
+import img5 from '../assets/Notion.png';
+import img6 from '../assets/Currency.png';
+import img7 from '../assets/Blog.png';
+import img8 from '../assets/Timeline-calendar.png';
+
 
 const ProjectsList = [
     {
@@ -11,7 +15,6 @@ const ProjectsList = [
         link: 'https://do-to-do.vercel.app/',
         github: 'https://github.com/HarshhMehta/To-do',
         image: img1
-        
     },
     {
         id: 2,
@@ -41,7 +44,28 @@ const ProjectsList = [
         github: 'https://github.com/HarshhMehta/Notion-Clone',
         image: img5
     },
+    {
+        id: 6,
+        title: 'Currrency-converter',
+        link: 'https://reall-time-currency-converter.vercel.app/',
+        github: 'https://github.com/HarshhMehta/currency-converter',
+        image: img6
+    },
+    {
+        id: 7,
+        title: 'Blog-app',
+        link: 'https://blog-app-1mxl.vercel.app/',
+        github: 'https://github.com/HarshhMehta/blog-app',
+        image: img7
+    },
+    {
+        id: 8,
+        title: 'Timeline-Calendar',
+        link: 'https://timeline-calendar-events.vercel.app/',
+        github: 'https://github.com/HarshhMehta/Timeline-calendar',
+        image: img8
+    }
     
-]
+];
 
-export default ProjectsList
+export default ProjectsList;
